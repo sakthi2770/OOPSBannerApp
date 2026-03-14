882aaa2999c3817eb847dd19da6bf57fb0fc3d14
sakthi2770 <sr2770@srmist.edu.in>
2026-03-14 13:09:32 +0530

    Create ArrayInit.java

---
+++ ArrayInit.java
@@ -0,0 +1,20 @@
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+      
+        String[] bannerLines = new String[] {
+            String.join(" ", "  ***", "  *** ", " ****", "  ****"),
+            String.join(" ", " *   *", "*   *", "*     *", "* "),
+            String.join(" ", " *   *", "*   *", "*     *", "* "),
+            String.join(" ", " *   *", "*   *", "* ***", "  ****"),
+            String.join(" ", " *   *", "*   *", "* ", "        *"),
+            String.join(" ", " *   *", "*   *", "* ", "        *"),
+            String.join(" ", "  ***", "  *** ", "* ", "     ****")
+        };
+
+        
+        for (String line : bannerLines) {
+            System.out.println(line);
+        }
+    }
+}
